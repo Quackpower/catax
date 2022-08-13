a87de1bfc6f3cbb84b1d3e9f5c27c0d53e3f55d4
Quackpower <quackpower70@gmail.com>
2022-08-12 20:43:54 -0500

    ok

--- static/src/js/ol_layout260721.js
+++ static/src/js/ol_layout260721.js
@@ -143,6 +143,87 @@ function traerMapa(con_capa){
         MapLayer.setZIndex(999);
         map.addLayer(MapLayer);
     }
+
+
+    map.on('singleclick', function(evt) {
+        if(document.getElementsByName("longitud")[0].tagName=="INPUT"){
+            var feature = map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+                return feature;
+            });
+    
+            var coordinate = ol.proj.toLonLat(evt.coordinate);
+            var prettyCoord = ol.proj.transform(evt.coordinate, 'EPSG:900913', 'EPSG:4326')
+    
+            var urlCal = 'https://nominatim.openstreetmap.org/search.php?q=' + prettyCoord[1] + '%2C%20' + prettyCoord[0] + '&addressdetails=1&format=jsonv2';
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("GET", urlCal, false);
+            xmlHttp.send(null);
+    
+            var createCharge = JSON.parse(xmlHttp.responseText);
+    
+            map.getLayers().forEach(layer => {
+                try {
+                    if (layer.get('name') && layer.get('name') == 'MapLayer') {
+                        map.removeLayer(layer)
+                    }
+                } catch (ex) {}
+            });
+    
+                if (feature.get('desc') != undefined) {
+    
+                } else {
+                    
+    
+                    $('[name*="calle"]').val(createCharge[0]['address']['road'])
+                    $('[name*="colonia"]').val(createCharge[0]['address']['neighborhood'])
+    
+                    direccion = createCharge[0]['display_name'];
+    
+                    $('[name*="latitud"]').val(prettyCoord[1])
+                    $('[name*="longitud"]').val(prettyCoord[0])
+    
+                    localStorage.setItem('latitud', prettyCoord[1]);
+                    localStorage.setItem('longitud', prettyCoord[0]);
+    
+                    var MarkerIcon = new ol.Feature({
+                        geometry: new ol.geom.Point(ol.proj.fromLonLat([prettyCoord[0], prettyCoord[1]])),
+                        name: 'MapLayer',
+                        desc: '<label>Details</label>'
+                    })
+                    MarkerIcon.setStyle(new ol.style.Style({
+                        image: new ol.style.Icon({
+                            anchor: [0.5, 50],
+                            anchorXUnits: 'fraction',
+                            anchorYUnits: 'pixels',
+                            src: '/catax/static/images/marcador-de-p.png'
+                        })
+                    }));
+    
+                    var MapSource = new ol.source.Vector({
+                        features: [
+                            MarkerIcon
+                        ]
+                    })
+    
+                    var MapLayer = new ol.layer.Vector({
+                        source: MapSource
+                    });
+    
+                    MapLayer.setZIndex(0);
+                    MapLayer.set('name', 'MapLayer');
+                    //MapLayer.setZIndex(999);
+                    map.addLayer(MapLayer);
+    
+                    
+    
+                    if(onchangenedded && dotrdy){
+                        dotrdy = false;
+                        $(".ref").val($(".ref").val()+" ").trigger('change'); //horrible hack para mandar un value a guardar y que tome todos los datos del mapa
+                    }
+                }
+            
+        }
+    });
     
 
     
