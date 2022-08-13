3484f7ee22f56de7f6965c6b0eac30d4a21b73de
Quackpower <quackpower70@gmail.com>
2022-08-12 20:17:36 -0500

    ok

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "reporte_ciudadano_coatepec_camas",
+    'name': "reporte_ciudadano_coatepec_cmas",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
